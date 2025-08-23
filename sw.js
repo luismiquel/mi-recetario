c0a417f21fce52c9a8ad87279fd92b14b55dd7b1
luismiquel <luismiguelgarciadelasmorenas@gmail.com>
2025-08-23 16:38:12 +0200

    PWA completa desde cero: manifest+icons+favicons+SW offline

--- sw.js
+++ sw.js
@@ -1,3 +1,37 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => clients.claim());
-// (sin caché por ahora)
+﻿const CACHE = "recetario-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./favicon.svg",
+  "./favicon.ico",
+  "./icons/icon-16.png",
+  "./icons/icon-32.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  // Cache-first para recursos del propio origen
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(res => res || fetch(e.request))
+    );
+  }
+});
