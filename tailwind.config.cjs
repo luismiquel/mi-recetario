fc0211383ef0f3412f95c1013cf35045ee55ac6f
Luis Miguel <luismiguelgarciadelasmorenas@gmail.com>
2025-09-07 19:42:54 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", "ui-sans-serif", "system-ui", "-apple-system", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "Liberation Sans", "sans-serif"]
-      }
-    }
-  },
-  darkMode: "class",
+  content: ["./index.html", "./**/*.js"],
+  theme: { extend: {} },
   plugins: []
 };
